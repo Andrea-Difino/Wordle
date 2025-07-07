0c2db0bec5ceb2da02192c9e2f06cebc7113af92
Andrea-Difino <adifino13@gmail.com>
2025-07-07 22:54:06 +0200

    netlify folder

--- netlify/functions/get-word.js
+++ netlify/functions/get-word.js
@@ -1,26 +1,18 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-exports.handler = async function(event, context) {
-  try {
-    const filePath = path.join(__dirname, 'parole5lettere.txt');
-    console.log(__dirname)
-    const parole = fs.readFileSync(filePath, 'utf8').split('\n').map(p => p.trim());
+exports.handler = async () => {
+  const filePath = path.join(__dirname, 'parole5lettere.json');
+  const raw = fs.readFileSync(filePath, 'utf8');
+  const wordList = JSON.parse(raw); // ottieni l'array
 
-    const random = parole[Math.floor(Math.random() * parole.length)];
+  const random = wordList[Math.floor(Math.random() * wordList.length)];
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        secret: random.toUpperCase(),
-        wordList: parole
-      }),
-    };
-  } catch (err) {
-    console.error('Errore funzione get-word:', err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      secret: random.toUpperCase(),
+      wordList: wordList.map(w => w.toUpperCase()),
+    }),
+  };
 };
